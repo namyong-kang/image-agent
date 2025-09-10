2594e32a7178965abba5c656633a2f7de5278c5a
mnlee-google <mnlee@google.com>
2025-09-11 01:48:51 +0900

    agent.py

--- image_agent/agent.py
+++ image_agent/agent.py
@@ -1,13 +1,31 @@
 from google.adk.agents import Agent
-from .prompt import agent_instruction
+from .prompt import (image_agent_instruction, architecture_agent_instruction, root_agent_instruction)
 
 #from google.adk.tools import google_search
 from .tools import generate_edit_image
 
-root_agent = Agent(
+
+image_agent = Agent(
     name="image_agent",
     model="gemini-2.5-flash",
     description="An agent responsible for generating and modifying images",
-    instruction=agent_instruction,
+    instruction=image_agent_instruction,
     tools=[generate_edit_image],
+)
+
+"""
+architecture_agent = Agent(
+    name="architecture_agent",
+    model="gemini-2.5-flash",
+    description="This agent provides a detailed breakdown and comprehensive explanation of system architecture diagrams",
+    instruction=architecture_agent_instruction,
+)
+"""
+root_agent = Agent(
+    name="root_agent",
+    model="gemini-2.5-flash",
+    description="An AI agent designed for image-related tasks, including content analysis, generation, and modification",
+    instruction=root_agent_instruction,
+    #sub_agents=[image_agent,architecture_agent]
+    sub_agents=[image_agent]
 )
